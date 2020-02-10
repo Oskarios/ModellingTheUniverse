bc45723e3c7b0a9fdade12f1a5905b24443f5ee9
Oskarios <47861292+Oskarios@users.noreply.github.com>
2020-02-10 18:22:45 +0000

    OO and Total Energy
    Started converting model to utilise object oriented programming paradigm and also added methods for calculating total energy to track consistency of system

--- skeleton.py
+++ skeleton.py
@@ -6,31 +6,93 @@ dt = 0.001 # timestep
 step = 1 # loop counter
 maxstep = 100000
 
+#Create class for star so that it can be used as a target object later on
+class Star:
+    def __init__(self, mass, initpos, radius):
+        #Currently taking stars as not having velocities so it has no such property
+        self.mass = mass
+        self.initpos = initpos
+        self.pos = self.initpos
+        self.radius = radius
+        #Main reason for having separate class is line below -- should create superclass for which subclasses override this line
+        self.sphere=sphere(pos=self.initpos, radius=self.radius, color=color.yellow)
+
+
+#Created a reusable class for 
+class Planet:
+    def __init__(self, mass, initpos, vel, radius):
+        self.mass = mass
+        self.initpos = initpos
+        self.vel = vel
+        self.radius = radius
+        self.pos = self.initpos
+        self.sphere = sphere(pos=self.initpos, radius=self.radius*self.mass,color=color.blue)
+        self.trace = curve(radius = 0.005, color = color.white)
+
+    def updateSphere(self):
+        self.sphere.pos = self.pos
+
+    #Performs necessary positional updates for the planet
+    def updatePos(self):
+        self.pos = self.pos + self.vel * dt
+        self.updateSphere()
+        self.updateTrace()
+
+    def updateTrace(self):
+        self.trace.append(self.pos)
+
+    #Returns kinetic energy of the planet
+    def getKE(self):
+        ke = 0.5 * self.mass * mag(self.vel)**2
+        return ke
+
+    #Returns the potential energy of the planet with respect to target celestial body object
+    def getPE(self, target):
+        pe = (self.mass * target.mass)/mag(self.pos - target.pos)
+        return pe
+
+
+
+#This class will control the forces present between the bodies within the solar system
+class SolarSystem:
+    def __init__(self, planets):
+        self.planets = planets
+
+    def addPlanet(self, planet):
+        self.planets[] = planet
+
+
+    ## REQUIRED METHODS ##
+
+        # CALCULATE FORCES BETWEEN BODIES IN SYSTEM
+    # I DON'T THINK THIS IS GOING TO WORK
+    def calcForce(self, body, bodyInField):
+        denom = mag(bodyInField.pos - body.pos) ** 3
+        dv = dt * body.pos * bodyInField.mass / denom
+        return dv
+        # CALCULATE TOTAL KINETIC ENERGIES OF BODIES IN SYSTEM
+        # CALCULATE TOTAL POTENTIAL ENERGIES OF BODIES IN SYSTEM
+        # CALCULATE TOTAL ENERGY OF SYSTEM
+
+
 #  Define the star, planets and constants
 M = 1000 # mass of star (G == 1)
-m1 = 1 # mass of planet 1
-initpos1 = vector(0,1,0) # initial position vector of Planet1
-Planet1 = sphere(pos=initpos1,radius=0.05*m1,color=color.blue)
-Star = sphere(pos=vector(0,0,0),radius=0.1,color=color.yellow)
-vel1 = -vector(25, 0, 0) # initial velocity of planet 1
+#Star = sphere(pos=vector(0,0,0),radius=0.1,color=color.yellow)
+
+Star = Star(M, vector(0,0,0), radius=0.1)
 
 
-#Set up rough secondary planet
-m2 = 2
-initpos2 = vector(0,1.5,0)
-Planet2 = sphere(pos=initpos2, radius=0.05*m2, color=color.green)
-vel2 = -vector(15, 0, 0)
 
-# Added trace for planet
-trace1 = curve(radius = 0.005, color = color.white)
+#Create first planet with OO approach
+Planet1 = Planet(1,vector(0,1,0), -vector(25,0,0), 0.05)
+#Create second planet with OO approach
+Planet2 = Planet(2,vector(0,1.5,0), -vector(15,0,0), 0.05)
 
-#Added trace for planet 2
-trace2 = curve(radius = 0.005, color = color.green)
 
 while step <= maxstep:
 
     rate(100)  # slow down the animation
-    print (Planet1.pos)
+    #print (Planet1.pos)
 
     # calculate changes in velocities
 
@@ -44,25 +106,34 @@ while step <= maxstep:
 
     #Planet1 on Planet 2
     denom12 = mag(Planet2.pos - Planet1.pos) ** 3
-    dv12 = dt * Planet1.pos * m2 / denom12
+    dv12 = dt * Planet1.pos * Planet2.mass / denom12
 
     
     #Planet2 on Planet1
     denom21 = mag(Planet1.pos - Planet2.pos) ** 3
-    dv21 =  dt * Planet2.pos * m2 / denom21
+    dv21 =  dt * Planet2.pos * Planet1.mass / denom21
     
     
 
     #update velocities
-    vel1 = vel1 - dv1M - dv21 + dv12
-    vel2 = vel2 - dv2M - dv12 + dv21
+    Planet1.vel += - dv1M - dv21
+    Planet2.vel += - dv2M - dv12
     
     # update positions
-    Planet1.pos = Planet1.pos + vel1 * dt
-    Planet2.pos = Planet2.pos + vel2 * dt
+    Planet1.updatePos()
+    Planet2.updatePos()
+    
+    #Calculate total KE
+    Total_KE = Planet1.getKE() + Planet2.getKE()
 
-    step = step + 1
-    trace1.append(Planet1.pos)
-    trace2.append(Planet2.pos)
+    #Calculate total potential energy
+    Total_PE = Planet1.getPE(Planet2) + Planet2.getPE(Planet1) + Planet1.getPE(Star) + Planet2.getPE(Star)
 
+    #Calculate the total energy of the solar system (the Hamiltonian)
+    Total_Energy = Total_KE - Total_PE
+
+    print(Total_Energy)
+
+    step += 1
+    
 print("end of program")
