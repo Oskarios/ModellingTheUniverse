779c7dfddcf659348909e82c7cd13b37ab64a0ab
Oskarios <47861292+Oskarios@users.noreply.github.com>
2020-02-10 14:35:43 +0000

    Added interplanetary forces
    Adding a second planet and the forces between them as required -- need to still check energies of individual planets and total system in order to validate physics

--- skeleton.py
+++ skeleton.py
@@ -4,7 +4,7 @@ from vpython import *
 
 dt = 0.001 # timestep
 step = 1 # loop counter
-maxstep = 1000
+maxstep = 100000
 
 #  Define the star, planets and constants
 M = 1000 # mass of star (G == 1)
@@ -14,8 +14,18 @@ Planet1 = sphere(pos=initpos1,radius=0.05*m1,color=color.blue)
 Star = sphere(pos=vector(0,0,0),radius=0.1,color=color.yellow)
 vel1 = -vector(25, 0, 0) # initial velocity of planet 1
 
+
+#Set up rough secondary planet
+m2 = 2
+initpos2 = vector(0,1.5,0)
+Planet2 = sphere(pos=initpos2, radius=0.05*m2, color=color.green)
+vel2 = -vector(15, 0, 0)
+
 # Added trace for planet
-trace = curve(radius = 0.005, color = color.white)
+trace1 = curve(radius = 0.005, color = color.white)
+
+#Added trace for planet 2
+trace2 = curve(radius = 0.005, color = color.green)
 
 while step <= maxstep:
 
@@ -23,16 +33,36 @@ while step <= maxstep:
     print (Planet1.pos)
 
     # calculate changes in velocities
+
+    #Planet1 Star
     denom1M = mag(Planet1.pos) ** 3 
     dv1M = dt * Planet1.pos * M / denom1M
+
+    #Planet2 Star
+    denom2M = mag(Planet2.pos)** 3
+    dv2M = dt * Planet2.pos * M / denom2M
+
+    #Planet1 on Planet 2
+    denom12 = mag(Planet2.pos - Planet1.pos) ** 3
+    dv12 = dt * Planet1.pos * m2 / denom12
+
     
-    # update velocities
-    vel1 = vel1 - dv1M
+    #Planet2 on Planet1
+    denom21 = mag(Planet1.pos - Planet2.pos) ** 3
+    dv21 =  dt * Planet2.pos * m2 / denom21
+    
+    
+
+    #update velocities
+    vel1 = vel1 - dv1M - dv21 + dv12
+    vel2 = vel2 - dv2M - dv12 + dv21
     
     # update positions
     Planet1.pos = Planet1.pos + vel1 * dt
+    Planet2.pos = Planet2.pos + vel2 * dt
 
     step = step + 1
-    trace.append(Planet1.pos)
+    trace1.append(Planet1.pos)
+    trace2.append(Planet2.pos)
 
 print("end of program")
